85336f9bdc752542c064ac2d54a42339c1e0035a
Jeremy <jeremy.durrieu@gmail.com>
2026-02-01 14:21:29 +0100

    Two Sum

---
+++ src/fr/jeremy/leetcode/Main.java
@@ -0,0 +1,12 @@
+package fr.jeremy.leetcode;
+
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        //Replace this with the Leetcode problem Solution method
+        System.out.println(Arrays.toString(TwoSum.twoSum(new int[]{2, 7, 11, 15}, 9)));
+    }
+}
